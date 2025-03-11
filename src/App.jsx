7b2dfd509eec2d5593b7c4cb013284bf2fa0b631
SockRat1 <johnbenson@tutamail.com>
2025-03-11 16:40:27 +0500

    refactored, put delete function and toggle function in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,46 @@ import { useState } from "react";
 import styles from "./App.module.css";
 import TodoHeader from "./components/TodoHeader";
 import TodoList from "./components/TodoList";
-import CompletedTasks from "./components/CompletedTasks";
 
 function App() {
   const [value, setValue] = useState("");
   const [getTodos, setTodos] = useState([]);
   const [nextId, setNextId] = useState(0);
+
   function addHadler() {
     setTodos([...getTodos, { id: nextId, text: value, isChecked: false }]);
     setNextId((prev) => prev + 1);
     setValue("");
   }
+
   function onReset() {
     setTodos([]);
   }
+
   function onClear() {
     const arr = getTodos.filter((todo) => !todo.isChecked);
     setTodos(arr);
   }
+
+  function toggleTodo(i) {
+    setTodos(
+      getTodos.map((val, index) => {
+        return {
+          ...val,
+          isChecked: index === i ? !val.isChecked : val.isChecked,
+        };
+      })
+    );
+  }
+
+  function deleteTodo(i) {
+    setTodos((prev) => {
+      const arr = [...prev];
+      arr.splice(i, 1);
+      return arr;
+    });
+  }
+
   return (
     <div className={styles.app}>
       <TodoHeader
@@ -29,8 +51,11 @@ function App() {
         onReset={onReset}
         onClear={onClear}
       ></TodoHeader>
-      <TodoList todos={getTodos} setTodos={setTodos} />
-      <CompletedTasks></CompletedTasks>
+      <TodoList
+        todos={getTodos}
+        deleteTodo={deleteTodo}
+        toggleTodo={toggleTodo}
+      />
     </div>
   );
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,31 +1,13 @@
 import TodoItem from "./TodoItem";
 
-function Todolist({ todos, setTodos }) {
-  function toggleChecked(i) {
-    setTodos(
-      todos.map((val, index) => {
-        return {
-          ...val,
-          isChecked: index === i ? !val.isChecked : val.isChecked,
-        };
-      })
-    );
-  }
-  function deletefn(i) {
-    setTodos((prev) => {
-      const arr = [...prev];
-      arr.splice(i, 1);
-      return arr;
-    });
-  }
-
+function Todolist({ todos, deleteTodo, toggleTodo }) {
   return todos.map((el) => {
     return (
       <TodoItem
         key={el.id}
         isChecked={el.isChecked}
-        toggleChecked={() => toggleChecked(el.id)}
-        deletefn={() => deletefn(el.id)}
+        toggleChecked={() => toggleTodo(el.id)}
+        deletefn={() => deleteTodo(el.id)}
       >
         {el.text}
       </TodoItem>
